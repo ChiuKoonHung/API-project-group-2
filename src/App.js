6e5d2831cf54972739ce0f1c1207cac567fd25ff
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2021-10-08 18:35:28 +0800

    czy_chart1_finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./component/chart_1_view";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React-code
-                </a>
-            </header>
+            <h1>Group_2 dash board</h1>
+            <div className="container">
+                <div className="chart">
+                    <Weather />
+                </div>
+                <div className="chart">2</div>
+                <div className="chart">3</div>
+                <div className="chart">4</div>
+                <div className="chart">5</div>
+                <div className="chart">6</div>
+            </div>
         </div>
     );
 }
