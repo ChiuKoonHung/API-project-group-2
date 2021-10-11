1cb5a3af7e2aa5cc2e43660bf28bad5d96fede0a
QuirkiDigital <46080052+QuirkiDigital@users.noreply.github.com>
2021-10-11 21:11:39 +0800

    2nd Commit LY

--- src/component/chart_4_view.js
+++ src/component/chart_4_view.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
+import ShowForecast from "./ly-chart-4/ly_forecast";
 
 export default function Forecast() {
-
-    
 
     return (
         <div className="App">
--- src/component/ly-chart-4/ly_forecast.js
+++ src/component/ly-chart-4/ly_forecast.js
@@ -1,32 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 //import "./ly_forecast.css";
 import API from "./API_forecast";
 document.title = "Forecast App";
 
-const ShowForecast = ({}) => (
-    
-    getWeatherForecast = (async e =>{
+const ShowForecast = ({}) => {
+
+    let [isLightOn, updateStatus] = useState();
+
+    useEffect(()=>{
+        
+        const getWeatherForecast = async (e) =>{
     
         //let query = "2021-11-10";
-        const forecast2h = await API.get(/2-hour-weather-forecast?date=2021-10-11);
-        const forecast24h = await API.get(/24-hour-weather-forecast?date=2021-10-11); 
-        const forecast4day = await API.get(/4-day-weather-forecast?date=2021-10-11); 
+        const forecast2h = await API.get("/2-hour-weather-forecast?date=2021-10-11");
+        const forecast24h = await API.get("/24-hour-weather-forecast?date=2021-10-11"); 
+        const forecast4day = await API.get("/4-day-weather-forecast?date=2021-10-11"); 
         //const designs = await API.get(`/designs?lodgement_date=${query}`);
         //const trademark = await API.get(`/trademarks?lodgement_date=${query}`);
          
     
-        console.log(forecast2h.status);
-        console.log(forecast24h.status);
-        console.log(forecast4day.status);
+        console.log("LY2HrForecast", forecast2h.status);
+        console.log("LY24HrForecast", forecast24h.status);
+        console.log("LY4dayForecast", forecast4day.status);
     
     
         // console.log(designs.data.count);
         // console.log(designs.data.items[0]["journals"]); //finding index 0 of "journals"
         // console.log(trademark.data.count);
     
-    })();
+    }
+    getWeatherForecast();
+    
+    },[])
 
-)
+    return (
+        <h1>Test</h1>
+    )
+    
+}
 
 export default ShowForecast;
 
