72db9d3c673965f520b6ea0b9d4ae9d13a9b1808
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2021-10-12 12:46:07 +0800

    changed to axios

--- src/component/czy_Chart/chart_1_view.js
+++ src/component/czy_Chart/chart_1_view.js
@@ -1,4 +1,5 @@
 // import "./App.css";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import WeatherCard from "./czy_weather";
 import { Dimmer, Loader } from "semantic-ui-react";
@@ -19,15 +20,23 @@ export default function Weather() {
                 });
             });
             console.log(geoLocation);
-            await fetch(
-                `${process.env.REACT_APP_API_URL}/weather/?lat=${geoLocation.lat}&lon=${geoLocation.long}&appid=${process.env.REACT_APP_API_KEY}`
-            )
-                .then((res) => res.json()) //here comsume the value
-                .then((result) => {
-                    //here set the result
-                    setData(result);
-                    console.log(result);
+            await axios
+                .get(
+                    `${process.env.REACT_APP_API_URL}/weather/?lat=${geoLocation.lat}&lon=${geoLocation.long}&appid=${process.env.REACT_APP_API_KEY}`
+                )
+                .then((res) => {
+                    console.log(res.data);
+                    setData(res.data);
                 });
+            // await fetch(
+            //     `${process.env.REACT_APP_API_URL}/weather/?lat=${geoLocation.lat}&lon=${geoLocation.long}&appid=${process.env.REACT_APP_API_KEY}`
+            // )
+            //     .then((res) => res.json()) //here comsume the value
+            //     .then((result) => {
+            //         //here set the result
+            //         setData(result);
+            //         console.log(result);
+            //     });
         };
         fetchData();
     }, []);
