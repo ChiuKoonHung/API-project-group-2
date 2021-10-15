6bf10796f22f5073489f7306c30cd6603ba66c90
Edison Zhuang <edisonzhuang@pop-os.localdomain>
2021-10-15 18:31:30 +0800

    Pure components for justin and liang yi

--- src/component/ly-chart-4/chart_4_view.js
+++ src/component/ly-chart-4/chart_4_view.js
@@ -1,11 +1,79 @@
+import React, { useEffect, useState } from "react";
 import ShowForecast from "./ly_forecast";
+import API from "../../API";
+document.title = "Forecast App";
 
 export default function Forecast() {
+    
+    const dateToday = new Date().toLocaleDateString('en-CA');
+    const timeNow = new Date().toLocaleTimeString('en-GB');
+    const dateTime = (dateToday+"T"+timeNow);
+ 
+    const [forecast2h, setForecast2h] = useState([]);
+    const [location2h, setLocation2h] = useState([]);
 
-    return (
-        <div className="App">
-            <ShowForecast />
-        </div>
-    );
+    const [forecast24h, setForecast24h] = useState([]);
+    const [north, setNorth] = useState([]);
+    const [south, setSouth] = useState([]);
+    const [east, setEast] = useState([]);
+    const [west, setWest] = useState([]);
+    const [central, setCentral] = useState([]);
+
+    const [forecast1st, setForecast1st] = useState([]);
+    const [date1st, setDate1st] = useState([]);
+
+    const [forecast2nd, setForecast2nd] = useState([]);
+    const [date2nd, setDate2nd] = useState([]);
+
+    const [forecast3rd, setForecast3rd] = useState([]);
+    const [date3rd, setDate3rd] = useState([]);
+
+    const [forecast4th, setForecast4th] = useState([]);
+    const [date4th, setDate4th] = useState([]);
+    const [areaIndex, setAreaIndex] = useState("0"); 
+
+    useEffect(()=>{
+        
+        const getWeatherForecast = async (e) =>{
+
+        try {
+
+            const forecast2h = await API.get(`2-hour-weather-forecast?date_time=${dateTime}`);
+            const forecast24h = await API.get(`24-hour-weather-forecast?date=${dateToday}`); 
+            const forecast4day = await API.get(`4-day-weather-forecast?date=${dateToday}`);  
+
+            setLocation2h(forecast2h.data.items[0]["forecasts"][areaIndex]["area"]);
+            setForecast2h(forecast2h.data.items[0]["forecasts"][areaIndex]["forecast"]);
+
+            setForecast24h(forecast24h.data.items[0]["general"]["forecast"]);
+            setNorth(forecast24h.data.items[0]["periods"][0]["regions"]["north"]);
+            setSouth(forecast24h.data.items[0]["periods"][0]["regions"]["south"]);
+            setEast(forecast24h.data.items[0]["periods"][0]["regions"]["east"]);
+            setWest(forecast24h.data.items[0]["periods"][0]["regions"]["west"]);
+            setCentral(forecast24h.data.items[0]["periods"][0]["regions"]["central"]);
+
+            setForecast1st(forecast4day.data.items[0]["forecasts"][0]["forecast"]);
+            setDate1st(forecast4day.data.items[0]["forecasts"][0]["date"]);
+
+            setForecast2nd(forecast4day.data.items[0]["forecasts"][1]["forecast"]);
+            setDate2nd(forecast4day.data.items[0]["forecasts"][1]["date"]);
+
+            setForecast3rd(forecast4day.data.items[0]["forecasts"][2]["forecast"]);
+            setDate3rd(forecast4day.data.items[0]["forecasts"][2]["date"]);
+            
+            setForecast4th(forecast4day.data.items[0]["forecasts"][3]["forecast"]);
+            setDate4th(forecast4day.data.items[0]["forecasts"][3]["date"]);
+
+        } catch(err){
+            console.log(err);
+        }    
+    }
+
+    getWeatherForecast();
+    
+    },[forecast2h, areaIndex])
+    //putting areaIndex in [] allows react to check for updates to areaIndex and update data accordingly (location and forecast)
+
+    return <ShowForecast massiveProps={{dateToday, timeNow, dateTime, forecast2h, location2h, forecast24h, north, south, east, west, central, forecast1st, date1st, forecast2nd, date2nd, forecast3rd, date3rd, forecast4th, date4th, setAreaIndex}} />
 
 }
--- src/component/ly-chart-4/ly_forecast.js
+++ src/component/ly-chart-4/ly_forecast.js
@@ -3,79 +3,23 @@ import "./ly_forecast.css";
 import API from "../../API";
 document.title = "Forecast App";
 
-const ShowForecast = ({}) => {
+/*
+    This is your pure component.
+
+    In this component, it is possible to break into even smaller components. FOr example, in your widget there are 4 parts
+    1. 24 hour forecast
+    2. 24 hour forecast by region
+    3. 2 hour forecast by location
+    4. 4 day forecast islandwide
+
+    This <ShowForecast> component could have broken into 4 smaller components, then it will be rendered in chart_4_view.js 
+
+    For now, I am breaking this up as first layer for you to have a sense of what is pure component. This component is now pure component, but not so reusable until you break it into 4.
+*/
+const ShowForecast = ({massiveProps}) => {
+    const {dateToday, timeNow, dateTime, forecast2h, location2h, forecast24h, north, south, east, west, central, forecast1st, date1st, forecast2nd, date2nd, forecast3rd, date3rd, forecast4th, date4th, setAreaIndex} = massiveProps;    
     
-    const dateToday = new Date().toLocaleDateString('en-CA');
-    const timeNow = new Date().toLocaleTimeString('en-GB');
-    const dateTime = (dateToday+"T"+timeNow);
- 
-    const [forecast2h, setForecast2h] = useState([]);
-    const [location2h, setLocation2h] = useState([]);
-
-    const [forecast24h, setForecast24h] = useState([]);
-    const [north, setNorth] = useState([]);
-    const [south, setSouth] = useState([]);
-    const [east, setEast] = useState([]);
-    const [west, setWest] = useState([]);
-    const [central, setCentral] = useState([]);
-
-    const [forecast1st, setForecast1st] = useState([]);
-    const [date1st, setDate1st] = useState([]);
-
-    const [forecast2nd, setForecast2nd] = useState([]);
-    const [date2nd, setDate2nd] = useState([]);
-
-    const [forecast3rd, setForecast3rd] = useState([]);
-    const [date3rd, setDate3rd] = useState([]);
-
-    const [forecast4th, setForecast4th] = useState([]);
-    const [date4th, setDate4th] = useState([]);
-    const [areaIndex, setAreaIndex] = useState("0"); 
-
-    useEffect(()=>{
-        
-        const getWeatherForecast = async (e) =>{
-
-        try {
-
-            const forecast2h = await API.get(`2-hour-weather-forecast?date_time=${dateTime}`);
-            const forecast24h = await API.get(`24-hour-weather-forecast?date=${dateToday}`); 
-            const forecast4day = await API.get(`4-day-weather-forecast?date=${dateToday}`);  
-
-            setLocation2h(forecast2h.data.items[0]["forecasts"][areaIndex]["area"]);
-            setForecast2h(forecast2h.data.items[0]["forecasts"][areaIndex]["forecast"]);
-
-            setForecast24h(forecast24h.data.items[0]["general"]["forecast"]);
-            setNorth(forecast24h.data.items[0]["periods"][0]["regions"]["north"]);
-            setSouth(forecast24h.data.items[0]["periods"][0]["regions"]["south"]);
-            setEast(forecast24h.data.items[0]["periods"][0]["regions"]["east"]);
-            setWest(forecast24h.data.items[0]["periods"][0]["regions"]["west"]);
-            setCentral(forecast24h.data.items[0]["periods"][0]["regions"]["central"]);
-
-            setForecast1st(forecast4day.data.items[0]["forecasts"][0]["forecast"]);
-            setDate1st(forecast4day.data.items[0]["forecasts"][0]["date"]);
-
-            setForecast2nd(forecast4day.data.items[0]["forecasts"][1]["forecast"]);
-            setDate2nd(forecast4day.data.items[0]["forecasts"][1]["date"]);
-
-            setForecast3rd(forecast4day.data.items[0]["forecasts"][2]["forecast"]);
-            setDate3rd(forecast4day.data.items[0]["forecasts"][2]["date"]);
-            
-            setForecast4th(forecast4day.data.items[0]["forecasts"][3]["forecast"]);
-            setDate4th(forecast4day.data.items[0]["forecasts"][3]["date"]);
-
-        } catch(err){
-            console.log(err);
-        }    
-    }
-
-    getWeatherForecast();
-    
-    },[forecast2h, areaIndex])
-    //putting areaIndex in [] allows react to check for updates to areaIndex and update data accordingly (location and forecast)
-
-    return (
-    <>    
+    return (    
     <div class = "main-container">
         <div class>
             <p>Today's date is {dateToday}. The time now is {timeNow}</p>
@@ -132,8 +76,7 @@ const ShowForecast = ({}) => {
         <p>{date4th}: {forecast4th}</p>
         </div>
 
-    </div> 
-    </>
+    </div>     
     )
 }
 
